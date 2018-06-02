0e02b9a4c05b5d57441a86c7b4d9c47a0dd6a929
David Zubal <zubaldavid@gmail.com>
2018-06-02 01:48:59 -0700

    Implement Particles.js and center card.

--- src/App.js
+++ src/App.js
@@ -8,30 +8,36 @@ class App extends Component {
   render() {
     return (
       <div className='login-form' >
-      <Particles className='background'
-              params={{
-            		particles: {
-                  number : {
-                    value:100,
-            			line_linked: {
-            				shadow: {
-            					enable: true,
-            					color: "#3CA9D1",
-            					blur: 5
-            				}
-            			}
-                 }
-            		}
-            	}}
-              style={{width: '100%'}}
-
+      <Particles
+        params={{
+      		particles : {
+            number : {
+              value:80,
+            },
+            color:{
+              value:["#E86363","#556b2f", "#7C5852"]
+            },
+            size: {
+              value:4
+            },
+      			line_linked: {
+    				  speed:6,
+              value:100,
+    					enable: true,
+    					color: "#A9A9A9",
+              width:1,
+    					blur: 5
+      			}
+      		}
+      	}}
+        style={{width: '100%', height: '100%',position:'absolute'}}
       />
       <Grid textAlign='center'
       style={{ height: '100%' }}
       verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 450, maxHeight: 550 }}>
-      <Card className='userCard' href='/'centered >
-        <Image src='zubal.jpg' style={{size:200}}/>
+      <Grid.Column style={{ maxWidth: 550, maxHeight: 550, opacity:0.88,shadow:10}}>
+      <Card className='userCard' href='/' centered >
+        <Image src='zubal.jpg'  style={{height:'375px'}} />
         <Card.Content>
           <Card.Header>David Zubal</Card.Header>
           <Card.Meta>
@@ -39,20 +45,18 @@ class App extends Component {
               Developer.Designer.Learner
             </span>
           </Card.Meta>
-          <Card.Description>
-            I am student of computer science.
-          </Card.Description>
         </Card.Content>
-        <Card.Content >
-          <a>
-            <Icon name='github'/>
-              <a href='https://github.com/zubaldavid/' rel="noopener noreferrer">Github</a>
+        <Card.Content style={{float:'left',color: 'black'}}>
+          <Card.Meta>
+            <Icon name='github' style={{color:'gray'}}/>
+              <a href='https://github.com/zubaldavid/' >Github  </a>
+            <Icon name='linkedin square' style={{color:'gray'}}/>
+              <a href='https://www.linkedin.com/in/zubaldavid/' >Linkedin</a>
             <br/>
-            <Icon name='linkedin square'/>
-              <a href='https://www.linkedin.com/in/zubaldavid/' target="_blank">Linkedin</a>
-            <br/>
-            <Icon name='mail'/>zubaldavid@gmail.com
-          </a>
+            <Icon name='mail' style={{color:'gray'}}/>
+              <a href='mailto:zubaldavid@gmail.com'>zubaldavid@gmail.com</a>
+          <br/>
+          </Card.Meta>
         </Card.Content>
       </Card>
       </Grid.Column>
